1affc9eea42e59b9538660f0d2318a4dcc64a906
BIPLOB <developerbiplob.bd@gmail.com>
2021-09-16 12:41:06 +0600

    Model | Migration | Controller | Seeder | Factory Creating

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\Admin::factory(10)->create();
+
+        Admin::create([
+            'name' => 'Biplob',
+            'email' => 'biplob@gmial.com',
+            'password' => bcrypt('laravel'),
+        ]);
     }
 }
