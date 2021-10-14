49b34354af8e1f9b1595401d6e11d9d2ae887db3
BIPLOB <developerbiplob.bd@gmail.com>
2021-10-15 03:08:02 +0600

    Custom Pagination

--- resources/views/frontend/post/all_post.blade.php
+++ resources/views/frontend/post/all_post.blade.php
@@ -26,15 +26,22 @@
                 </div>
               </div>
             @endforeach
+            {{-- Customizing The Pagination View
+              // Ai comand run korte hobe  - php artisan vendor:publish --tag=laravel-pagination
+              // view er modde vendor/pagination akta folder provide kore se kane onek gulo Pagination dewa thake.
+              // And chaile amara o cusotm vabe create korte pari.
+              // 
+               --}}
+            {{ $posts->links('vendor.pagination.custom-pagination') }}
             </div>
             <!-- Pagination -->
             {{-- <nav aria-label="Page navigation example">
               <ul class="pagination pagination-template d-flex justify-content-center">
-                <li class="page-item"><a href="#" class="page-link"> <i class="fa fa-angle-left"></i></a></li>
+                <li class="page-item"><a href="{{ $paginator->previousPageUrl() }}" class="page-link"> <i class="fa fa-angle-left"></i></a></li>
                 <li class="page-item"><a href="#" class="page-link active">1</a></li>
                 <li class="page-item"><a href="#" class="page-link">2</a></li>
                 <li class="page-item"><a href="#" class="page-link">3</a></li>
-                <li class="page-item"><a href="#" class="page-link"> <i class="fa fa-angle-right"></i></a></li>
+                <li class="page-item"><a href="{{ $paginator->nextPageUrl() }}" class="page-link"> <i class="fa fa-angle-right"></i></a></li>
               </ul>
             </nav> --}}
           </div>
